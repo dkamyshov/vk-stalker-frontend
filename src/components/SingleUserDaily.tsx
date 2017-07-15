2a90ad91177fd89123a5f2cbaa6759907133a687
Danil Kamyshov <danil.kamyshov@gmail.com>
2017-07-15 15:02:25 +0300

    minor improvements

--- src/components/SingleUserDaily.tsx
+++ src/components/SingleUserDaily.tsx
@@ -89,6 +89,7 @@ class SingleUserDaily extends React.Component<any, any> {
             <div className='date-nav' id='date-select'>
                 <Link to={`/dashboard/${userId}`}>К списку дней</Link>
                 <Link to={`/dashboard/${userId}/${parseInt(offset)-day}/#date-select`}>← {fmtDate(new Date(parseInt(offset)-day))}</Link>
+                <b>{fmtDate(new Date(parseInt(offset)))}</b>
                 <Link to={`/dashboard/${userId}/${parseInt(offset)+day}/#date-select`}>{fmtDate(new Date(parseInt(offset)+day))} →</Link>
             </div>
             
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -9,11 +9,47 @@ const dateOptions = {
     minute: 'numeric',
     second: 'numeric'
 };
+
+const classByStatus = status => {
+    switch(status) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+            return `p${status}`;
+        default: return 'no';
+    }
+}
+
+const platformsDivs = platforms => {
+    let pf = Object.create(null);
+
+    platforms.map(platform => {
+        if(platform != 0) {
+            pf[platform] = (pf[platform] || 0) + 1;
+        }
+    });
+
+    let records = Object.keys(pf).sort((k1, k2) => pf[k2] - pf[k1]).map(k => ({platform: parseInt(k), records: pf[k]}));
+
+    return <div>
+        {
+            records.slice(0, 4).map(record => (
+                <div className={`status-chip ${classByStatus(record.platform)}`}>{record.records}</div>
+            ))
+        }
+    </div>;
+}
 class Statistics extends React.Component<any, any> {
     constructor(props) {
         super(props);
 
         this.state = {
+            topRecords: [],
             recordsCount: 0,
             usersCount: '---',
             accounts: [],
@@ -36,7 +72,7 @@ class Statistics extends React.Component<any, any> {
     }
 
     render() {
-        const {recordsCount, usersCount, accounts, log} = this.state;
+        const {recordsCount, usersCount, accounts, log, topRecords} = this.state;
 
         return <div className='container'>
             <p>
@@ -73,6 +109,42 @@ class Statistics extends React.Component<any, any> {
                                 <td>{account.users.length.toLocaleString('ru')}</td>
                                 <td>{account.settings.balance.toLocaleString('ru')}</td>
                                 <td>{account.settings.pause ? 'нет' : 'да'}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+
+            <p>По записям:</p>
+
+            <div className="text-center">
+                <div className="status-chip p1">m.vk.com</div>
+                <div className="status-chip p2">iPhone</div>
+                <div className="status-chip p3">iPad</div>
+                <div className="status-chip p4">Android</div>
+                <div className="status-chip p5">WP</div>
+                <div className="status-chip p6">Win10</div>
+                <div className="status-chip p7">vk.com</div>
+            </div>
+
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Имя</th>
+                        <th>Записей</th>
+                        <th>Платформы</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        topRecords.map(({_id, name, count, platforms}) => (
+                            <tr>
+                                <td><a href={`/dashboard/${_id}`}>{_id}</a></td>
+                                <td>{name}</td>
+                                <td>{count}</td>
+                                <td>{platformsDivs(platforms)}</td>
                             </tr>
                         ))
                     }
